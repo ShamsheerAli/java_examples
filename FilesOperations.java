3e9eceec405897909ba3728b81ab37fcbadcf2a2
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2021-10-15 23:30:31 +0530

    commited 10/14 at 11:05 PM

--- FilesOperations.java
+++ FilesOperations.java
@@ -2,8 +2,12 @@ package java_examples;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
 
 public class FilesOperations {
 
@@ -21,15 +25,30 @@ public class FilesOperations {
 	      }
 	      writer1.close();
 	      PrintWriter writer2 = new PrintWriter(new File("C:\\SHAMSHEER\\NumbersFile-2.txt"));
+	      Random rand2 = new Random();
 	      int number2, count2=0; 
-	      while(count!=200)
+	      while(count2!=400)
 	      {
-	            number2=rand.nextInt(200)+1;
+	            number2=rand2.nextInt(400)+1;
 	            writer2.print(number2);
 	            count2++;
 	      
 	         writer2.println();
 	      }
 	      writer2.close();
+	      String filePath = "C:\\SHAMSHEER\\NumbersFile-1.txt";
+	      String input = "";
+	      Scanner sc = new Scanner(new File(filePath));
+	      FileWriter writer = new FileWriter("C:\\SHAMSHEER\\NumbersFile-1.txt");
+	      Set set = new HashSet();
+	      while (sc.hasNextLine()) {
+	         input = sc.nextLine();
+	         if(set.add(input)) {
+	            writer1.append(input+"\n");
+	         }
+	      }
+	      writer1.flush();
+	      System.out.println("Contents added............");
+
 	}
 }
