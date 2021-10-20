8486ce9205cefd62f7c0ba845b63810b5591837a
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2021-10-20 22:56:56 +0530

    commited 10/20 at 10:56 PM

--- FilesOperations.java
+++ FilesOperations.java
@@ -16,20 +16,20 @@ import java.util.Set;
 public class FilesOperations {
 
 	public static void main(String[] args) throws Exception {
-		 PrintWriter writer1 = new PrintWriter(new File("C:\\SHAMSHEER\\NumbersFile-1.txt"));
+		 PrintWriter writer1 = new PrintWriter(new File("C:\\SHAMSHEER\\RandomNumList-1.txt"));
 	      Random rand = new Random();
 	      int number, count=0; 
-	      while(count!=200)
+	      while(count!=20)
 	      {
-	            number=rand.nextInt(200)+1;
+	            number=rand.nextInt(20)+1;
 	            writer1.print(number);
 	            count++;
 	      
 	         writer1.println();
 	      }
 	      writer1.close();
-	      Scanner scanner = new Scanner(new File("C:\\SHAMSHEER\\NumbersFile-1.txt"));
-	      int [] duplicates1 = new int [200];
+	      Scanner scanner = new Scanner(new File("C:\\SHAMSHEER\\RandomNumList-1.txt"));
+	      int [] duplicates1 = new int [20];
 	      Set<Integer> duplicatesSet1=new HashSet<>();
 	      List<Integer> duplicatesList1=new ArrayList<>();
 	      int i = 0;
@@ -44,26 +44,26 @@ public class FilesOperations {
 	      duplicatesList1.addAll(duplicatesSet1);
 	      Collections.sort(duplicatesList1);
 	      //System.out.println("--------------------"+duplicatesList1);
-	      FileWriter newWriter=new FileWriter("C:\\SHAMSHEER\\NumbersFile-3.txt");
+	      FileWriter newWriter=new FileWriter("C:\\SHAMSHEER\\UniqueNumList-1.txt");
 	      for (Integer values1: duplicatesList1) {
 	    	  newWriter.write(values1+System.lineSeparator());
 	      }
 	      newWriter.close();
-	      System.out.println("The No.of Elements in File 3: "+ duplicatesList1.size());
-	      PrintWriter writer2 = new PrintWriter(new File("C:\\SHAMSHEER\\NumbersFile-2.txt"));
+	      System.out.println("Unique Numbers In File-1: "+ duplicatesList1.size());
+	      PrintWriter writer2 = new PrintWriter(new File("C:\\SHAMSHEER\\RandomNumList-2.txt"));
 	      Random rand2 = new Random();
 	      int number2, count2=0;
-	      while(count2!=400)
+	      while(count2!=40)
 	      {
-	            number2=rand2.nextInt(400)+1;
+	            number2=rand2.nextInt(40)+1;
 	            writer2.print(number2);
 	            count2++;
 	      
 	         writer2.println();
 	      }
 	      writer2.close();
-	      Scanner sc = new Scanner(new File("C:\\SHAMSHEER\\NumbersFile-2.txt"));
-	      int [] duplicates2 = new int [400];
+	      Scanner sc = new Scanner(new File("C:\\SHAMSHEER\\RandomNumList-2.txt"));
+	      int [] duplicates2 = new int [40];
 	      Set<Integer> duplicatesSet2=new HashSet<>();
 	      List<Integer> duplicatesList2=new ArrayList<>();
 	      int a = 0;
@@ -78,23 +78,47 @@ public class FilesOperations {
 	      duplicatesList2.addAll(duplicatesSet2);
 	      Collections.sort(duplicatesList2);
 	    //  System.out.println("--------------------"+duplicatesList2);
-	      FileWriter newWriter2=new FileWriter("C:\\SHAMSHEER\\NumbersFile-4.txt");
+	      FileWriter newWriter2=new FileWriter("C:\\SHAMSHEER\\UniqueNumList-2.txt");
 	      for (Integer values: duplicatesList2) {
 	    	  newWriter2.write(values+System.lineSeparator());
 	      }
-	      System.out.println("The NO.of Elements in File 4: "+ duplicatesList2.size());
+	      System.out.println("Unique Numbers in File 2: "+ duplicatesList2.size());
 	      newWriter2.close();
 	      
-	      Set<Integer> finalSet=new HashSet<>();
-	      finalSet.addAll(duplicatesList1);
-	      finalSet.addAll(duplicatesList2);
-	    //  System.out.println("Final"+finalSet);
-	      FileWriter newWriter3=new FileWriter("C:\\SHAMSHEER\\NumbersFile-5.txt");
-	      for (Integer values: finalSet) {
+	      List<Integer> finalList=new ArrayList<>();
+	      for(Integer values: duplicatesList1) {
+	    	  if(duplicatesList2.contains(values)) {
+	    	finalList.add(values);
+	      }
+	      }
+	      FileWriter newWriter3=new FileWriter("C:\\SHAMSHEER\\CommonNumList.txt");
+	      for (Integer values: finalList) {
 	    	  newWriter3.write(values+System.lineSeparator());
 	      }
 	      newWriter3.close();
-	      System.out.println("The No.of Elements in File 5: "+ finalSet.size());
-	      System.out.println("Done");
+	      System.out.println("Numbers That Are In both file: "+ finalList.size());
+	      
+	      List<Integer> duplicateofboth=new ArrayList<>();
+	      for(Integer values: duplicatesList2) {
+	    	  if(duplicatesList1.contains(values)) {
+	    		  duplicateofboth.add(values);
+	    	  }
+	      }
+	      List<Integer> uniquesInFile1=new ArrayList<>();
+	      List<Integer> uniquesInFile2=new ArrayList<>();
+	      for(Integer values: duplicatesList1) {
+	    	  if(duplicateofboth.contains(values)) {
+	    		  continue;
+	    	  }else
+	    		  uniquesInFile1.add(values);
+	      }
+	      for(Integer values: duplicatesList2) {
+	    	  if(duplicateofboth.contains(values)) {
+	    		  continue;
+	    	  }else
+	    		  uniquesInFile2.add(values);
+	      }
+	      System.out.println("Numbers That Are In File-1,But not In File-2: "+uniquesInFile1.size());
+	      System.out.println("Numbers That Are In File-2,But not In File-1: "+uniquesInFile2.size());
 	}
 }
