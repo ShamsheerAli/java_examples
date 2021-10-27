3b9d6189a8cec165f0d353775decfdfe01333bcd
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2021-10-27 22:28:04 +0530

    commited 10/27 at 10:28 PM

--- JSONFile.java
+++ JSONFile.java
@@ -1,10 +1,21 @@
 package java_examples;
 
+import java.util.Scanner;
+
 public class JSONFile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Employee Name");
+        String emp_name=sc.nextLine();
+        System.out.println("Enter Emplyee Age");
+        String emp_age=sc.nextLine();
+        System.out.println("Enter Employee Phone Number");
+        String emp_num=sc.nextLine();
+        System.out.println("Enter Employee Email Id");
+        String emp_email=sc.nextLine();
+        
+        System.out.println(emp_name+"-"+emp_age+"-"+emp_num+"-"+emp_email);
 	}
 
 }
