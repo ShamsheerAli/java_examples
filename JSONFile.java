24f5b717c29b717cb65c26a3a8c9aa7e9f7684cc
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2021-10-28 22:40:46 +0530

    commited 10/28 at 10:40 PM

--- JSONFile.java
+++ JSONFile.java
@@ -1,35 +1,45 @@
 package java_examples;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class JSONFile {
 
-	public static void main(String[] args) {
-       /* Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		PrintWriter writer=new PrintWriter("C:\\SHAMSHEER\\Emp_JSON.txt");
+		writer.write("{\n");
+		writer.write("\t"+"\""+"Employee_Data:"+"\""+" "+"[\n");
+		String option="";
+         do {
+        Scanner sc=new Scanner(System.in);
         System.out.println("Enter Employee Name");
         String emp_name=sc.nextLine();
-        System.out.println("Enter Emplyee Age");
+        System.out.println("Enter Employee Age");
         String emp_age=sc.nextLine();
         System.out.println("Enter Employee Phone Number");
         String emp_num=sc.nextLine();
         System.out.println("Enter Employee Email Id");
-        String emp_email=sc.nextLine();*/
+        String emp_email=sc.nextLine();
         
-        System.out.println("{");
-        System.out.println("\t"+"\""+"Employee_Data:"+"\""+" "+"[");
-        
-        System.out.println("\t\t {");
-       /* Map<String, String> details=new HashMap<>();
+        writer.write("\t\t {\n");
+        Map<String, String> details=new HashMap<>();
         details.put("Name", emp_name);
         details.put("Age", emp_age);
         details.put("Phone", emp_num);
-        details.put("Email", emp_email);*/
-        System.out.println("\t\t },");
+        details.put("Email", emp_email);
+        for (Map.Entry<String, String> map : details.entrySet()) {
+			writer.write("\t\t\t"+"\""+ map.getKey()+"\""+" : " + "\"" + map.getValue()+"\"\n");
+		}
+        writer.write("\t\t },\n");
         
-        System.out.println("\t]");
-        System.out.println("}");
+        writer.write("\t]\n");
+        writer.write("}\n");
+        System.out.println("Do You want enter Details(Y or N)");
+        option=sc.next();
+         }while(option=="N"||option=="n");
+         writer.close();
 	}
-
 }
