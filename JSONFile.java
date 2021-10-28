595f723eb1daf7508a5dd1f71d1db65a1ef32a6e
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2021-10-28 21:19:43 +0530

    commited 10/28 at 09:19 PM

--- JSONFile.java
+++ JSONFile.java
@@ -1,11 +1,13 @@
 package java_examples;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class JSONFile {
 
 	public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+       /* Scanner sc=new Scanner(System.in);
         System.out.println("Enter Employee Name");
         String emp_name=sc.nextLine();
         System.out.println("Enter Emplyee Age");
@@ -13,9 +15,21 @@ public class JSONFile {
         System.out.println("Enter Employee Phone Number");
         String emp_num=sc.nextLine();
         System.out.println("Enter Employee Email Id");
-        String emp_email=sc.nextLine();
+        String emp_email=sc.nextLine();*/
         
-        System.out.println(emp_name+"-"+emp_age+"-"+emp_num+"-"+emp_email);
+        System.out.println("{");
+        System.out.println("\t"+"\""+"Employee_Data:"+"\""+" "+"[");
+        
+        System.out.println("\t\t {");
+       /* Map<String, String> details=new HashMap<>();
+        details.put("Name", emp_name);
+        details.put("Age", emp_age);
+        details.put("Phone", emp_num);
+        details.put("Email", emp_email);*/
+        System.out.println("\t\t },");
+        
+        System.out.println("\t]");
+        System.out.println("}");
 	}
 
 }
