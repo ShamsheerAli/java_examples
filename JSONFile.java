9adfb655e97134602eb7f94249c8d6590e53b6b0
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2021-10-29 22:54:11 +0530

    commited 10/29 at 10:54 PM

--- JSONFile.java
+++ JSONFile.java
@@ -1,9 +1,12 @@
 package java_examples;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class JSONFile {
@@ -11,35 +14,34 @@ public class JSONFile {
 	public static void main(String[] args) throws Exception {
 		PrintWriter writer=new PrintWriter("C:\\SHAMSHEER\\Emp_JSON.txt");
 		writer.write("{\n");
-		writer.write("\t"+"\""+"Employee_Data:"+"\""+" "+"[\n");
-		String option="";
-         do {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Employee Name");
-        String emp_name=sc.nextLine();
-        System.out.println("Enter Employee Age");
-        String emp_age=sc.nextLine();
-        System.out.println("Enter Employee Phone Number");
-        String emp_num=sc.nextLine();
-        System.out.println("Enter Employee Email Id");
-        String emp_email=sc.nextLine();
-        
-        writer.write("\t\t {\n");
-        Map<String, String> details=new HashMap<>();
-        details.put("Name", emp_name);
-        details.put("Age", emp_age);
-        details.put("Phone", emp_num);
-        details.put("Email", emp_email);
-        for (Map.Entry<String, String> map : details.entrySet()) {
-			writer.write("\t\t\t"+"\""+ map.getKey()+"\""+" : " + "\"" + map.getValue()+"\"\n");
+		writer.write("\t"+"\""+"Employee_Data"+"\""+":"+" "+"[\n");
+		Scanner reader=new Scanner(new File("C:\\SHAMSHEER\\Emp_Data2.txt"));
+	    String data="";
+	    List<String> list=new ArrayList<>();
+	    while(reader.hasNext()) {
+	    	data=reader.nextLine();
+	    	list.add(data);
+	    }
+	    String a="";
+		String[] fileData=list.toArray(new String[0]);
+		for(String value: fileData)
+			a+=value;
+		String[] fileDetails=a.split(",");
+		String[] emp_details2=Arrays.copyOfRange(fileDetails, 4, fileDetails.length);
+	
+		String[] emp_details=Arrays.copyOfRange(fileDetails, 0, 4);
+		System.out.println(emp_details.length);
+		int count=1;
+		for(int i=0;i<emp_details2.length;i++) {
+			if(i==0||i%4==0)
+				 writer.write("\t\t {\n");
+			writer.write("\t\t\t"+"\""+ emp_details[i]+"\""+" : " + "\"" + emp_details2[i]+"\"\n");
+			if(count%4==0)
+				 writer.write("\t\t },\n");
+			count++;
 		}
-        writer.write("\t\t },\n");
-        
         writer.write("\t]\n");
         writer.write("}\n");
-        System.out.println("Do You want enter Details(Y or N)");
-        option=sc.next();
-         }while(option=="N"||option=="n");
          writer.close();
-	}
+         	}
 }
