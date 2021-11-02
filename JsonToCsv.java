95ef09b794790dc04f268d7a6d7250fd7d3eda6b
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2021-11-02 22:32:57 +0530

    commited 11/02 at 10:32 PM

--- JsonToCsv.java
+++ JsonToCsv.java
@@ -1,10 +1,16 @@
 //Program to Convert CSV File to JSON File
 package java_examples;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class JsonToCsv {
 
-	public static void main(String[] args) {
-	 
+	public static void main(String[] args) throws Exception {
+	 Scanner sc=new Scanner(new File("C:\\SHAMSHEER\\Emp_JSON.txt"));
+	 String json=sc.nextLine();
+	 System.out.println(sc);
 	}
 
 }
