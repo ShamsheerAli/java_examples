821b65cf448a87a0a160365ff0598f24f2438aca
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2021-11-07 21:52:45 +0530

    commited 11/07 at 09:52 PM

--- JsonToCsv.java
+++ JsonToCsv.java
@@ -1,17 +1,28 @@
 //Program to Convert CSV File to JSON File
 package java_examples;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.File; 
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class JsonToCsv {
 
 	public static void main(String[] args) throws Exception {
-	 Scanner sc=new Scanner(new File("C:\\SHAMSHEER\\Emp_JSON.txt"));
-	 String json=sc.nextLine();
-	 System.out.println(sc);
-	 System.out.println(json);
-	 
+		BufferedReader reader = new BufferedReader(new FileReader("C:\\SHAMSHEER\\Emp_JSON.txt"));
+		StringBuilder stringBuilder = new StringBuilder();
+		String line = null;
+		String ls = System.getProperty("line.separator");
+		while ((line = reader.readLine()) != null) {
+			stringBuilder.append(line);
+			stringBuilder.append(ls);
+		}
+		// delete the last new line separator
+		stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+		reader.close();
+
+		String content = stringBuilder.toString();
+		System.out.println(content);
 	}
-}
+} 
