1941d245841cfd65ef22877a57d42e0c26691912
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2021-11-12 22:20:40 +0530

    commited 11/12 at 10:20 PM

--- JsonToCsv.java
+++ JsonToCsv.java
@@ -1,47 +1,9 @@
  //Program to Convert CSV File to JSON File
 package java_examples;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class JsonToCsv {
 	public static void main(String[] args) throws Exception {
-		BufferedReader reader = new BufferedReader(new FileReader("C:\\SHAMSHEER\\Emp_JSON.txt"));
-		StringBuilder stringBuilder = new StringBuilder();
-		String line = null;
-		String ls = System.getProperty("line.separator");
-		while ((line = reader.readLine()) != null) {
-			stringBuilder.append(line);
-			stringBuilder.append(ls);
-		}
-		stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-		reader.close();
-		String content = stringBuilder.toString();
-		content=content.replace("\t", "");
-		content=content.replace("[", "");
-		content=content.replace("]", "");
-		content=content.replace("{", "");
-		content=content.replace("}", "");
-		content=content.replace("\"", "");
-		content=content.replace(",", "");
-		content=content.replace(" ", "");
-		
-		String[] data=content.split(":");
-		String[] data2=new String[data.length];
-		List<String> dataList=new ArrayList<>();
-		List<String> dataList2=new ArrayList<>();
-		  for(int i=0; i<data.length; i++) {
-			  dataList.add(data[i]);    
-          }
-		  for(int i=0;i<data.length;i++) {
-			  if(data[i].contains("Email"))
-				  dataList2.add(data[i]);
-		  }
-		  System.out.println(dataList2);
+	
 	}
 }
