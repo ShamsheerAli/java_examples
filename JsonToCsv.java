59b788445dc79538a70f2557c92bfcf3e8a8b680
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2021-11-14 23:08:10 +0530

    commited 11/14 at 11:08 PM

--- JsonToCsv.java
+++ JsonToCsv.java
@@ -1,24 +1,25 @@
- //Program to Convert Json File to Csv File
+//Program to Convert Json File to Csv File
 package java_examples;
+
 import java.io.File;
 import java.util.Scanner;
 
 public class JsonToCsv {
 	public static void main(String[] args) throws Exception {
-		String str1="";
-		String str2="";
-		String str3="";
-		int count=0;
-		Scanner reader=new Scanner(new File("C:\\SHAMSHEER\\Emp_JSON.txt"));   
+		String str1 = "";
+		String str2 = "";
+		String str3 = "";
+		int count = 0;
+		Scanner reader = new Scanner(new File("C:\\SHAMSHEER\\Emp_JSON.txt"));
 		while (reader.hasNextLine()) {
 			reader.toString();
-		 str1 = reader.nextLine();
-		 str2=str1;
-		 str3=str2;
-		 if(str3==":")
-			 count++;
+			str1 = reader.nextLine();
+			str2 = str1;
+			str3 = str2;
+			if (str3 == ":")
+				count++;
 		}
-		 System.out.println(count);
-		 System.out.println(str3);
-		}
+		System.out.println(count);
+		System.out.println(str3);
+	}
 }
