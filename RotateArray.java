5d79ec089d227441c81c21db85b33291f712ea35
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2022-01-03 22:04:46 +0530

    03/01/2022 at 10:04 PM

--- RotateArray.java
+++ RotateArray.java
@@ -1,10 +1,30 @@
 package java_examples;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class RotateArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+           Scanner sc=new Scanner(System.in);
+           System.out.println("Enter The Size Of The Array");
+           int num=sc.nextInt();
+           System.out.println("Enter The Rotation Number");
+           int m=sc.nextInt();
+           int[] a=new int[num];
+           System.out.println("Enter The Elements Of Array");
+           for(int i=0; i<num; i++) 
+        	   a[i]=sc.nextInt();
+           List<Integer> mylist1=new ArrayList<>();
+           List<Integer> mylist2=new ArrayList<>();
+           for(int i=0; i<m; i++) 
+        	   mylist1.add(a[i]);
+           for(int i=m; i<a.length; i++)
+        		   mylist2.add(a[i]);
+           for(int i=0; i<mylist1.size(); i++) 
+        	   mylist2.add(mylist1.get(i));
+           System.out.println(mylist2);
 	}
 
 }
