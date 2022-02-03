a5f6728081506ac2b6645479eb213c0467a2a463
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2022-02-03 21:43:50 +0530

    03/02/2022 at 09:43 PM

--- MaxDiff.java
+++ MaxDiff.java
@@ -2,9 +2,27 @@ package java_examples;
 
 public class MaxDiff {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	int maxDiff(int arr[], int arr_size)
+    {
+        int max_diff = arr[1] - arr[0];
+        int i, j;
+        for (i = 0; i < arr_size; i++)
+        {
+            for (j = i + 1; j < arr_size; j++)
+            {
+                if (arr[j] - arr[i] > max_diff)
+                    max_diff = arr[j] - arr[i];
+            }
+        }
+        return max_diff;
+    }
+ 
+    /* Driver program to test above functions */
+    public static void main(String[] args)
+    {
+        MaxDiff maxdif = new MaxDiff();
+        int arr[] = {2,3,10,6,4,8,1};
+        System.out.println("Maximum difference is " +
+                                maxdif.maxDiff(arr, 5));
+    }
 }
