fe64a5d76f27c96ff9168bd0c0954f98d6ca2115
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2022-02-20 23:51:09 +0530

    20/02/2022 at 11:51 PM

--- Segregate0and1.java
+++ Segregate0and1.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Segregate0and1 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in); 
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter The Size OF Array");
 		int num = sc.nextInt();
 		int[] a = new int[num];
@@ -16,16 +16,17 @@ public class Segregate0and1 {
 			a[i] = sc.nextInt();
 		segregate(a);
 	}
-    public static void segregate(int[] a) {
-    	List<Integer> list1=new ArrayList<>();  
-    	List<Integer> list2=new ArrayList<>();
-    	for(int i=0; i<a.length; i++) {
-    		if(a[i]==0)
-    			list1.add(a[i]);
-    		else
-    			list2.add(a[i]);
-    	}
-    	list1.addAll(list2);
-    	System.out.println(list1);
-    }
+
+	public static void segregate(int[] a) {
+		List<Integer> list1 = new ArrayList<>();
+		List<Integer> list2 = new ArrayList<>();
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == 0)
+				list1.add(a[i]);
+			else
+				list2.add(a[i]);
+		}
+		list1.addAll(list2);
+		System.out.println(list1);
+	}
 }
