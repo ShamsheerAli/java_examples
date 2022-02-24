17c0f5b71c90e2e10d6728bda6b6a958067253b9
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2022-02-24 23:47:02 +0530

    24/02/2022 at 11:47 PM

--- Ceiling.java
+++ Ceiling.java
@@ -1,9 +1,7 @@
 package java_examples;
 
 import java.util.Scanner;
-
 public class Ceiling {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter The Value You Want");
