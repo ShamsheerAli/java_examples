71bc6255424a6f11bea9be89710747b1ae91abc3
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2022-02-25 23:16:32 +0530

    25/02/2022 at 11:16 PM

--- ArrayPuzzle.java
+++ ArrayPuzzle.java
@@ -1,10 +1,34 @@
 package java_examples;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ArrayPuzzle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Size OF Array");
+		int num = sc.nextInt();
+		int[] a = new int[num];
+		System.out.println("Enter The Elements In Array!");
+		for (int i = 0; i < a.length; i++)
+			a[i] = sc.nextInt();
+		puzzle(a);
+	}
+	public static void puzzle(int[] a) {
+		List<Integer> list=new ArrayList<>();
+		for(int i=0; i<a.length; i++) {
+			int count=1;
+			for(int j=0; j<a.length;j++) {
+				if(i==j)
+					continue;
+				else
+					count=count*a[j];
+			}
+			list.add(count);
+		}
+		System.out.println(list);
 	}
 
 }
