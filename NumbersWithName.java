b9de4e2f08699830f4f51c07c6b9488c7e977716
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2022-04-10 22:43:46 +0530

    10/04/2022 at 10:43 PM

--- NumbersWithName.java
+++ NumbersWithName.java
@@ -1,20 +1,32 @@
 package java_examples;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumbersWithName {
 
 	public static void main(String[] args) {
-		int number=100;
-		for(int i=1;i<=number;i++) {
-			
-			if(i%15==0)
-				System.out.println("shamsheer ali");
-			else if(i%3==0)
-				System.out.println("shamsheer");
-			else if(i%5==0)
-				System.out.println("ali");
-			 else {
-				 System.out.println(i);
-			 }	
+		String s1[]=solve(15);
+		for(int i=0; i<s1.length; i++)
+			System.out.println(s1[i]);
 		}
-	}
+	public static String[] solve(int n) {
+        List<String> a= new ArrayList<>();
+        String[] s=new String[n];
+        for(int i=1; i<n+1; i++){
+            if(i%3==0 && i%5==0){
+               a.add("FizzBuzz"); 
+            }else if(i%5==0){
+                a.add("Buzz");
+            }else if(i%3==0){
+                a.add("Fizz");
+            }else
+               a.add(Integer.toString(i));
+        }
+        for(int i=0; i<n; i++){
+            s[i]=a.get(i);
+        }
+        return s;
+    }
 }
+
