99d4580997aa640472a6d8c1fb7738765d6bac81
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2022-04-12 22:10:52 +0530

    12/04/2022 at 10:10 PM

--- MaxDiff.java
+++ MaxDiff.java
@@ -1,7 +1,6 @@
 package java_examples;
 
 public class MaxDiff {
-
 	int maxDiff(int arr[], int arr_size) {
 		int max_diff = arr[1] - arr[0];
 		int i, j;
@@ -13,7 +12,6 @@ public class MaxDiff {
 		}
 		return max_diff;
 	}
-
 	public static void main(String[] args) {
 		MaxDiff maxdif = new MaxDiff();
 		int arr[] = { 2, 3, 10, 6, 4, 8, 1 };
