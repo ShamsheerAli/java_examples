2ea51d7f37833232ad24ee408d8b4fee7e7a545e
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2022-04-16 11:19:35 +0530

    16/04/2022 at 11:19 AM

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+package java_examples;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String args[]){  
+		  int r,sum=0,temp;     
+		  Scanner sc= new Scanner(System.in);
+		  System.out.println("Enter The Number!!!!!!!!!11");
+		  int n=sc.nextInt(); 
+		  temp=n;    
+		  while(n>0){    
+		   r=n%10;  
+		   sum=(sum*10)+r;    
+		   n=n/10;    
+		  }    
+		  if(temp==sum)    
+		   System.out.println("palindrome number ");    
+		  else    
+		   System.out.println("not palindrome");    
+		}  
+}
