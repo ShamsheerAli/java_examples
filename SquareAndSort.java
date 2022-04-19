2ce03b5bd95094f890f9fe95b00f7fb653c90ecc
Shamsheer Ali Shaik <shamsheeralissa@gmail.com>
2022-04-19 22:31:18 +0530

    19/04/2022 at 10:31 PM

--- SquareAndSort.java
+++ SquareAndSort.java
@@ -6,22 +6,23 @@ import java.util.Scanner;
 public class SquareAndSort {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter The Size Of Array");
-		int n=sc.nextInt();
-		int[] a=new int[n];
-		for(int i=0; i<n; i++)
-			a[i]=sc.nextInt();
-		    a=solve(a);
-		for(int i=0; i<n; i++)
-           System.out.println(a[i]);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = sc.nextInt();
+		a = solve(a);
+		for (int i = 0; i < n; i++)
+			System.out.println(a[i]);
 	}
+
 	public static int[] solve(int[] nums) {
-        int[] a=new int[nums.length];
-        for(int i=0; i<nums.length; i++){
-            a[i]=nums[i]*nums[i];
-        }
-        Arrays.sort(a);
-        return a;
-    }
+		int[] a = new int[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			a[i] = nums[i] * nums[i];
+		}
+		Arrays.sort(a);
+		return a;
+	}
 }
